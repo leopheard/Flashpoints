57ff5ad37bdbfac48728e973dbd216bd167ba14b
leopheard <leopheard.misc@gmail.com>
2020-11-17 21:47:49 -0500

    upload

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 
 An award winning front-line investigative news magazine focusing on human, civil and workers rights, issues of war and peace, Global Warming, racism and poverty, and other issues. Hosted by <a href="http://dennisjbernstein.com/">Dennis J. Bernstein</a> and broadcast on Pacifica / KPFA.<br>
 
+- <a href="https://kpfa.org/program/flashpoints/">Flashpoints - KPFA site</a>
+- <a href="https://www.gregpalast.com">GregPalast.com</a>
+
 <a href="www.kodi.tv">Kodi</a> (formerly known as XBMC) is an award-winning free and open source (GPL) software media player and entertainment hub that can be installed on Linux, OSX, Windows, iOS and Android, featuring a 10-foot user interface for use with televisions and remote controls. It allows users to play and view most videos, music, podcasts, and other digital media files from local and network storage media and the internet.<br>
 
 <b>^^^^ To install this addon ^^^^</b>, either use the <a href="https://www.tvaddons.co/github-browser-kodi/">Kodi Github installer</a> addon or save the .zip file downloaded from the 'clone or download' button (on this page above) to somewhere the Kodi can access (e.g. network drive or USB stick). Then on the Kodi, go to addons > install from zip file.<br>
--- addon.py
+++ addon.py
@@ -3,6 +3,7 @@ from resources.lib import mainaddon
 
 plugin = Plugin()
 url1 = "https://kpfa.org/program/flashpoints/feed/"
+url2 = "https://feeds.soundcloud.com/users/soundcloud:users:225697835/sounds.rss"
 @plugin.route('/')
 def main_menu():
     items = [
@@ -11,9 +12,9 @@ def main_menu():
             'path': plugin.url_for('episodes1'),
             'thumbnail': "https://is2-ssl.mzstatic.com/image/thumb/Podcasts123/v4/69/8e/5a/698e5a63-aac1-0161-fcd8-b81e2a5fff25/mza_5594927251231214019.jpg/600x600bb.jpg"},
         {
-            'label': plugin.get_string(30000),
-            'path': plugin.url_for('episodes'),
-            'thumbnail': "https://is2-ssl.mzstatic.com/image/thumb/Podcasts123/v4/69/8e/5a/698e5a63-aac1-0161-fcd8-b81e2a5fff25/mza_5594927251231214019.jpg/600x600bb.jpg"},
+            'label': plugin.get_string(30002),
+            'path': plugin.url_for('episodes2'),
+            'thumbnail': "https://i1.sndcdn.com/avatars-000496287465-vei9oo-original.jpg"},
     ]
     return items
 
@@ -24,11 +25,11 @@ def episodes1():
     items = mainaddon.compile_playable_podcast1(playable_podcast1)
     return items
 
-@plugin.route('/episodes/')
-def episodes():
-    soup1 = mainaddon.get_soup1(url1)
-    playable_podcast = mainaddon.get_playable_podcast(soup1)
-    items = mainaddon.compile_playable_podcast(playable_podcast)
+@plugin.route('/episodes2/')
+def episodes2():
+    soup2 = mainaddon.get_soup2(url2)
+    playable_podcast2 = mainaddon.get_playable_podcast2(soup2)
+    items = mainaddon.compile_playable_podcast2(playable_podcast2)
     return items
 
 if __name__ == '__main__':
